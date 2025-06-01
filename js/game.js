32254107167de55e8d12933ea3efbebdf001e5b2
Sax Arch <maxsax.sch@gmail.com>
2025-06-01 21:43:41 +0200

    feat: setup basic js functionality

--- js/game.js
+++ js/game.js
@@ -1,3 +1,48 @@
+let list = [];
+let inputList = [];
+
 $(document).ready(function () {
-	console.log("ready");
+    $('.js--add').on('click', function() {
+	
+	// debug
+	console.log("list", list);
+	console.log("inputlist", inputList);
+
+        const input = $('#item').val().trim();
+
+        if (!checkInput(input)) {
+            alert("Game over! Fout antwoord.");
+            return;
+        }
+
+        if (inputList.length < list.length) {
+            addToInputList(input);
+        } else {
+            addToList(input);
+            inputList = [];
+            switchPlayer();
+        }
+
+        $('.item').val('');
+    });
 });
+
+function checkInput(input) {
+    if (inputList.length < list.length) {
+        return input === list[inputList.length];
+    }
+    return input.length > 0 && !list.includes(input);
+}
+
+function addToInputList(input) {
+    inputList.push(input);
+}
+
+function addToList(input) {
+    list.push(input);
+}
+
+function switchPlayer() {
+	$(".p1").toggle();
+	$(".p2").toggle();
+}
--- snippets/game.php
+++ snippets/game.php
@@ -1,8 +1,9 @@
 <div class="container">
-        <small>Beurt: <?= htmlspecialchars($player1) ?></small>
+        <small class="p1">Beurt: <?= htmlspecialchars($player1) ?></small>
+        <small class="p2">Beurt: <?= htmlspecialchars($player2) ?></small>
         <h1>Ik ga op reis en ik neem mee...</h1>
             <label for="item">Wat neem je mee?</label>
             <input type="text" class="big-input" id="item" name="item" autocomplete="off" autofocus>
             <br>
-            <button class="submit-btn" type="submit">Toevoegen</button>
+            <button class="submit-btn js--add">Toevoegen</button>
     </div>
